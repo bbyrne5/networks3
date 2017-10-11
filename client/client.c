d0969d71c45f52db4bd9e80bfadf5ae1e7aef7c5
David Ryan Mellitt <dmellitt@student00.cse.nd.edu>
2017-10-11 19:09:25 -0400

    a

---
+++ client/client.c
@@ -0,0 +1,343 @@
+/*
+ * client.c
+ * Brian Byrne bbyrne5, David Mellitt dmellitt
+ * TCP client implementing FTP
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define MAXDATASIZE 4096
+#define SMALLSIZE 256
+
+int delete(int s, char * name, char * type);
+char * yesOrNo(char * name);
+int lengthAndName(int s, char * buf);
+int download(int s, char * buf);
+int upload(int s, char * buf);
+int deleteFile(int s, char * buf);
+int list(int s, char * buf);
+int makeDir(int s, char * buf);
+int removeDir(int s, char * buf);
+int changeDir(int s, char * buf);
+int quit(int s);
+
+int main(int argc, char * argv[] )
+{
+  struct hostent *hp;
+  struct sockaddr_in sin;
+  char *host;
+  char buf[MAXDATASIZE];
+  int port, s;
+
+  if (argc != 3) {
+    fprintf(stderr, "usage: ./myftp host port\n");
+    exit(1);
+  }
+
+  host = argv[1];
+  port = atoi(argv[2]);
+
+  // translate host name into IP address
+  hp = gethostbyname(host);
+  if(!hp) {
+    fprintf(stderr, "client: unknown host: %s\n", host);
+    exit(1);
+  }
+
+  // build address
+  bzero((char *)&sin, sizeof(sin));
+  sin.sin_family = AF_INET;
+  bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+  sin.sin_port = htons(port);
+
+  // active open
+  if ((s=socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("client: socket error");
+    exit(1);
+  }
+
+  if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0){
+    perror("client: connect failed");
+    close(s);
+    exit(1);
+  }
+
+  int retv = 0;
+  while (1) {
+    printf("Operation: ");
+    fgets(buf, sizeof(buf), stdin);
+    buf[MAXDATASIZE-1] = '\0';
+    if      (!strncmp(buf,"DWLD\n",5)) retv = download(s, buf);
+    else if (!strncmp(buf,"UPLD\n",5)) retv = upload(s, buf);
+    else if (!strncmp(buf,"DELF\n",5)) retv = deleteFile(s, buf);
+    else if (!strncmp(buf,"LIST\n",5)) retv = list(s, buf);
+    else if (!strncmp(buf,"MDIR\n",5)) retv = makeDir(s, buf);
+    else if (!strncmp(buf,"RDIR\n",5)) retv = removeDir(s, buf);
+    else if (!strncmp(buf,"CDIR\n",5)) retv = changeDir(s, buf);
+    else if (!strncmp(buf,"QUIT\n",5)) {
+      retv = quit(s);
+      break;
+    }
+    else
+      printf("Unrecognized command.\n");
+
+    if(retv) break;
+  }
+
+  close(s);
+  printf("Session closed.\n");
+
+  return retv;
+}
+
+int delete(int s, char * name, char * type) {
+  short confirm = 0;
+  if (recv(s, &confirm, sizeof(confirm), 0)) {
+    perror("client: receive error");
+    return 1;
+  }
+  confirm = ntohs(confirm);
+
+  if(confirm < 0) {
+    printf("The %s does not exist on server\n", type);
+    return 0;
+  }
+
+  char * answer = yesOrNo(name);
+
+  if (send(s, answer, strlen(answer)+1, 0) == -1) {
+    perror("client: send error");
+    return 1;
+  }
+
+  if (!strncmp(answer,"No",2)) {
+    printf("Delete abandoned by the user!\n");
+    return 0;
+  }
+
+  if (recv(s, &confirm, sizeof(confirm), 0)) {
+    perror("client: receive error");
+    return 1;
+  }
+  confirm = ntohs(confirm); 
+
+  if(confirm < 0)
+    printf("Failed to delete directory\n");
+  else
+    printf("Deleted %s\n", type); 
+
+  return 0;
+}  
+
+char * yesOrNo(char * name) {
+
+  char buf[4];
+
+  while (1) {
+    printf("Are you sure you want to delete %s? (Yes\\No)\n", name);
+    fgets(buf, 4, stdin);
+    if (!strncmp(buf,"Yes\n",4))
+      return("Yes");
+    if (!strncmp(buf,"No\n",3))
+      return("No");   
+  }
+}
+
+int lengthAndName(int s, char * buf) {
+  fgets(buf, SMALLSIZE, stdin);
+  buf[SMALLSIZE-1] = '\0';
+
+  short len = strlen(buf);
+
+  // strip newline
+  if(buf[len-1] == '\n') {
+    buf[len-1] = '\0';
+    len--;
+  }
+
+  short sendNum = htons(len);
+  // send file length
+  if (send(s, &sendNum, sizeof(sendNum), 0) == -1 ) {
+    perror("client: send error");
+    return 1;
+  }
+
+  // send file name
+  if (send(s, buf, len + 1, 0) == -1 ) {
+    perror("client: send error");
+    return 1;
+  }
+
+  return 0;
+}
+
+int download(int s, char * buf) {
+  if (send(s, "DWLD", 5, 0) == -1) {
+    perror("client: send error");
+    return 1;
+  }
+
+  printf("File name: ");
+
+  if(lengthAndName(s, buf) == 1)
+    return 1;
+
+  short receiveNum = 0;
+  if (read(s, &receiveNum, sizeof(receiveNum)) == -1) {
+    perror("client: receive error");
+    return 1;
+  }
+  short fileLen = ntohs(receiveNum);
+  printf("%d\n", fileLen);
+
+  if (fileLen == -1) {
+    printf("File %s does not exist on server\n", buf);
+    return 0;
+  }
+
+  FILE * fp = fopen(buf, "w+");
+
+  int i;
+  for(i = 0; i < fileLen; i += MAXDATASIZE){
+    if (read(s, buf, MAXDATASIZE) == -1) {
+      perror("client: receive error");
+      return 1;
+    }
+    printf("%s\n", buf);
+    printf("%d\n", fileLen-i);
+    if (fileLen - i < MAXDATASIZE)
+      fwrite(buf, sizeof(char), fileLen-i, fp);
+    else
+      fwrite(buf, sizeof(char), MAXDATASIZE, fp);
+  }
+
+  fclose(fp);
+  return 0;
+}
+
+int upload(int s, char * buf) {
+  if (send(s, "UPLD", 5, 0) == -1) {
+    perror("client: send error");
+    return 1;
+  }
+
+  printf("File name: ");
+
+  if(lengthAndName(s, buf) == 1)
+    return 1;
+
+  return 0;
+}
+
+int deleteFile(int s, char * buf) {
+  if (send(s, "DELF", 5, 0) == -1) {
+    perror("client: send error");
+    return 1;
+  }
+
+  printf("File name: ");
+
+  if(lengthAndName(s, buf) == 1)
+    return 1;
+ 
+  delete(s, buf, "file");
+  return 0;
+}
+
+int list(int s, char * buf) {
+  if (send(s, "LIST", 5, 0) == -1) {
+    perror("client: send error");
+    return 1;
+  }
+
+  return 0;
+}
+
+int makeDir(int s, char * buf) {
+  if (send(s, "MDIR", 5, 0) == -1) {
+    perror("client: send error");
+    return 1;
+  }
+
+  printf("Directory name: ");
+
+  if(lengthAndName(s, buf) == 1)
+    return 1;
+
+  short receiveNum = 0;
+  if (recv(s, &receiveNum, sizeof(receiveNum), 0)) {
+    perror("client: receive error");
+    return 1;
+  }
+
+  receiveNum = ntohs(receiveNum);
+  if (receiveNum == -2)
+    printf("The directory already exists on server\n");
+  else if (receiveNum == -1)
+    printf("Error in making directory\n");
+  else if(receiveNum > 0)
+    printf("The directory was successfully made\n");
+
+  return 0;
+}
+
+int removeDir(int s, char * buf) {
+  if (send(s, "RDIR", 5, 0) == -1) {
+    perror("client: send error");
+    return 1;
+  }
+
+  printf("Directory name: ");
+
+  if(lengthAndName(s, buf) == 1)
+    return 1;
+
+  delete(s, buf, "directory");
+
+  return 0;
+}
+
+int changeDir(int s, char * buf) {
+  if (send(s, "CDIR", 5, 0) == -1) {
+    perror("client: send error");
+    return 1;
+  }
+
+  printf("Directory name: ");
+
+  if(lengthAndName(s, buf) == 1)
+    return 1;
+
+  short receiveNum = 0;
+  if (recv(s, &receiveNum, sizeof(receiveNum), 0)) {
+    perror("client: receive error");
+    return 1;
+  }
+
+  receiveNum = ntohs(receiveNum);
+  if (receiveNum == -2)
+    printf("The directory does not exist on server\n");
+  else if (receiveNum == -1)
+    printf("Error in changing directory\n");
+  else if(receiveNum > 0)
+    printf("Changed current directory\n");
+
+  return 0;
+}
+
+int quit(int s) {
+  if (send(s, "QUIT", 5, 0) == -1) {
+    perror("client: send error");
+    return 1;
+  }
+
+  return 0;
+}
---
+++ server/server.c
@@ -0,0 +1,304 @@
+/*
+ * server.cpp
+ * Brian Byrne bbyrne5, David Mellitt dmellitt
+ * TCP server that implements FTP
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <time.h>
+
+#define MAXDATASIZE 4096
+#define SMALLSIZE 256
+
+int download(int);
+int upload(int);
+int delete(int);
+int list(int);
+int makeDir(int);
+int removeDir(int);
+int changeDir(int);
+
+int main(int argc, char * argv[] )
+{
+  struct sockaddr_in sin, client_addr;
+  int rqst;
+  char buf[MAXDATASIZE];
+  int port, s;
+  socklen_t addr_len;
+
+  if (argc != 2) {
+    fprintf(stderr, "Usage: ./myftpd port\n");
+    exit(1);
+  }
+
+  port = atoi(argv[1]);
+
+  // build address
+  bzero((char *)&sin, sizeof(sin));
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = INADDR_ANY;
+  sin.sin_port = htons(port);
+
+  // passive open
+  if ((s=socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("Server Socket Error!\n");
+    exit(1);
+  }
+
+  int opt = 1;
+  setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(int));
+
+  if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+    perror("Server Bind Error!\n");
+    shutdown(s, SHUT_RDWR);
+    exit(1);
+  }
+
+  addr_len = sizeof(client_addr);
+  
+  if (listen(s, 5) < 0) {
+    perror("listen failed");
+    close(s);
+    exit(1);
+  }
+ 
+  int retv; 
+  while (1){
+    if ((rqst = accept(s, (struct sockaddr *)&client_addr, &addr_len)) < 0){
+        perror("server: accept failed");
+        close(s);
+        exit(1);
+    }
+
+    while(1) {
+      bzero((char*)&buf, sizeof(buf));
+
+      while(buf[0]== '\0') {
+        if(read(rqst, buf, SMALLSIZE) == -1) {
+          perror("server: receive failed");
+          close(s);
+          exit(1);
+        }
+      }
+
+      if (!strncmp(buf,"DWLD",4)) retv = download(rqst);
+      else if (!strncmp(buf,"UPLD",4)) retv = upload(rqst);
+      else if (!strncmp(buf,"DELF",4)) retv = delete(rqst);
+      else if (!strncmp(buf,"LIST",4)) retv = list(rqst);
+      else if (!strncmp(buf,"MDIR",4)) retv = makeDir(rqst);
+      else if (!strncmp(buf,"RDIR",4)) retv = removeDir(rqst);
+      else if (!strncmp(buf,"CDIR",4)) retv = changeDir(rqst);
+      else if (!strncmp(buf,"QUIT",4)) break;
+      else
+        printf("Unrecognized command %s.\n", buf);
+
+      if (retv == 1){
+        printf("error caused: closing connection\n");
+        break;
+      }
+    }
+    close(rqst);
+  }
+
+  return 0;
+}
+
+int download(int rqst) {
+  //get name length
+  short nameLength = 0;
+  long fileLength = 0;
+  if(read(rqst, &nameLength, sizeof(nameLength)) == -1) {
+    perror("server: read error");
+    return 1;
+  }
+  nameLength = ntohs(nameLength);
+
+  //get the name
+  char fileName[nameLength+1];
+  if(read(rqst, fileName, nameLength) == -1) {
+    perror("server: read error");
+    return 1;
+  }
+  fileName[nameLength] = '\0';
+
+  FILE * fp;
+  //check for file
+  if(access(fileName, F_OK) != -1){//file does exist
+    fp = fopen(fileName, "r");
+    fseek(fp, 0, SEEK_END);
+    fileLength = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+  }
+  else {//file does not exist
+    fileLength = -1;
+  }
+
+  // send length
+  if (send(rqst, &fileLength, sizeof(fileLength), 0) < 0 ) {
+    perror("server: send error");
+    return 1;
+  }
+  
+  printf("%lu\n", fileLength);
+
+  if(fileLength < 0){
+    return 0;
+  }
+
+  // send file
+  char buf[MAXDATASIZE];
+  int bytes;
+  while((bytes = fread(buf, sizeof(char), MAXDATASIZE, fp)) > 0){
+    printf("%d\n%s\n", bytes, buf);
+    if(send(rqst, buf, bytes, 0) < 0){
+      perror("server: send error");
+      return 1;
+    } 
+  }
+
+  fclose(fp);
+  return 0;
+}
+
+int upload(int rqst) {
+  return 0;
+}
+
+int delete(int rqst) {
+  //get name length
+  char buf[256];
+  int valread = read(rqst, buf, SMALLSIZE);
+  buf[valread] = 0;
+  int size = atoi(buf);
+
+  //get the name
+  char dirName[size+1];
+  valread = read(rqst, dirName, size);
+  dirName[valread] = 0;
+
+  //check for file
+  int success = 1;
+  int failure = -1;
+  if(access(dirName, F_OK) != -1){//file does exist
+    send(rqst, &success, sizeof(success), 0);
+    read(rqst, buf, SMALLSIZE);
+    unlink(dirName);
+  }
+  else {//file does not exist
+    send(rqst, &failure, sizeof(failure), 0);
+    printf("File does not exist");
+  }
+
+  return 0;
+}
+
+int list(int rqst) {
+  FILE* p = popen("ls -la", "r");
+  if (!p) return 1;
+  char buff[1024];
+  while (fgets(buff, sizeof(buff), p)) {
+    printf("%s", buff);
+  }
+  pclose(p);
+  int size = strlen(buff);
+  int tmp = htonl((uint32_t)size);
+  send(rqst, &tmp, sizeof(tmp), 0);
+  send(rqst, buff, strlen(buff), 0);
+  return 0;
+}
+
+int makeDir(int rqst) {
+  //get name length
+  int size;
+  int valread = read(rqst, &size, sizeof(size));
+
+  //get the name
+  char dirName[size+1];
+  valread = read(rqst, dirName, size);
+  dirName[valread] = 0;
+
+  struct stat st = {0};
+  //check if dir exists and create it if not
+  int success = 1;
+  int failure = -1;
+  int extraFailure = -2;
+  if (stat(dirName, &st) == -1){
+    int create = mkdir(dirName, 0700);
+    if (create > 0)
+      send(rqst, &success, sizeof(success), 0); 
+    else 
+      send(rqst, &failure, sizeof(failure), 0);
+  } 
+  else {
+    send(rqst, &extraFailure, sizeof(extraFailure), 0);
+  } 
+
+  return 0;
+}
+
+int removeDir(int rqst) {
+  //get name length
+  int32_t size;
+  int valread = read(rqst, &size, sizeof(size));
+
+  //get the name
+  char dirName[size+1];
+  valread = read(rqst, dirName, size);
+  dirName[valread] = 0;
+
+  struct stat st = {0};
+  //check if dir exists and create it if not
+  int success = 1;
+  int failure = -1;
+  int extraFailure = -2;
+  if (stat(dirName, &st) == -1){
+    send(rqst, &extraFailure, sizeof(extraFailure), 0);
+  } 
+  else {
+    int create = mkdir(dirName, 0700);
+    if (create > 0)
+      send(rqst, &success, sizeof(success), 0); 
+    else 
+      send(rqst, &failure, sizeof(failure), 0);
+  } 
+
+  return 0;
+}
+
+int changeDir(int rqst) {
+  //get name length
+  int32_t size;
+  int valread = read(rqst, &size, sizeof(size));
+
+  //get the name
+  char dirName[size+1];
+  valread = read(rqst, dirName, size);
+  dirName[valread] = 0;
+  
+  struct stat st = {0};
+  //check if dir exists and create it if not
+  int success = 1;
+  int failure = -1;
+  int extraFailure = -2;
+  if (stat(dirName, &st) == -1){
+    send(rqst, &extraFailure, sizeof(extraFailure), 0);
+  }
+  else {
+    int change = chdir(dirName);
+    if (change > 0)
+      send(rqst, &success, sizeof(success), 0); 
+    else 
+      send(rqst, &failure, sizeof(failure), 0);
+  }
+  return 0;
+}
+
